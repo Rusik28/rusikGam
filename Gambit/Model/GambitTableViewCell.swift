24ecca5dfd0d8893a8da641bc74adecf367b3e8a
Ruslan ALiev <rusik_aliev_2002@mail.ru>
2022-03-17 18:20:42 +0300

    commit7

--- Gambit/Model/GambitTableViewCell.swift
+++ Gambit/Model/GambitTableViewCell.swift
@@ -13,8 +13,76 @@ class GambitTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var menuButton: UIButton!
     
-     public var number = 0
+    
+    public var number = 0
+    let defaults = UserDefaults.standard
+    var id = 0
+    static let identifier = "gambitCellOne"
+
+    func set(object gambit: Gambit) {
+         self.nameLabel.text = gambit.name
+         self.priceLabel.text = "\(gambit.price) ₽"
+         self.imageViewOne.downloaded(from: gambit.image)
+         self.id = gambit.id
+}
+     
+    
+    func plus(_ plus: Bool) {
+        if plus { number += 1 }
+        else { number -= 1 }
+    }
+    
+    func buttonA(_ action: Bool){
+      menuButton.isHidden = action
+  }
+    
+    func saveCount( _ countFood: Int) {
+        numberLabel.text = String(countFood)
+        defaults.set(countFood, forKey: String(id))
+    }
+    
+    @IBAction func plusMenu(_ sender: UIButton) {
+        plus(true)
+        saveCount(number)
+        buttonA(true)
+    }
+   
+    @IBAction func plusOne(_ sender: UIButton) {
+        number += 1
+        numberLabel.text = "\(number)"
+ //       plus(true)
+ //       saveCount(number)
+}
+    override func didMoveToWindow() {
+    super.didMoveToWindow()
+    numberLabel.text = "\(number)"
+        
+}
+   
+    @IBAction func minusOne(_ sender: UIButton) {
+        guard number > 0 else { return }
+            number -= 1
+            numberLabel.text = "\(number)"
+ //       plus(false)
+ //       saveCount(number)
+            if number == 0 {
+            buttonA(false)
+    }
+    }
+    
+    override func awakeFromNib() {
+           super.awakeFromNib()
+           numberLabel.text = "\(number)"
+       }
+    
+}
+    
+    
+    
 //     var button1: UIButton = {
 //            let button = UIButton()
 //            button.frame = CGRect(x: 300, y: 99, width: 32, height: 32)
@@ -49,57 +117,31 @@ class GambitTableViewCell: UITableViewCell {
 //    }
     
     
-    @IBAction func minusLabel(_ sender: UIButton) {
-        guard number > 0 else { return }
-        number -= 1
-        numberLabel.text = "\(number)"
-    }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        numberLabel.text = "\(number)"
-        
-    }
-   
-    @IBAction func plusButton(_ sender: UIButton) {
-        number += 1
-        numberLabel.text = "\(number)"
-    }
-
-     override func didMoveToWindow() {
-        super.didMoveToWindow()
-         numberLabel.text = "\(number)"
-    }
-        
-    
-    @IBAction func clear(_ sender: UIButton) {
-     //   if sender.tag == 1 && ( == 1) {
-   //             makeButtonSpawn2().isHidden = false
-
-    }
-    
-//    @objc func buttonClear(_ sender: UIButton) {
-//        if monkeyPosition == 1 {
-//            button1.isHidden = false
-//            button2.isHidden = false
-//        }
+//    @IBAction func minusLabel(_ sender: UIButton) {
+//        guard number > 0 else { return }
+//        number -= 1
+//        numberLabel.text = "\(number)"
 //    }
-    
-    
-    
-    
-    
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        numberLabel.text = "\(number)"
+//
+//    }
+//
+//    @IBAction func plusButton(_ sender: UIButton) {
+//        number += 1
+//        numberLabel.text = "\(number)"
+//    }
+//
+//     override func didMoveToWindow() {
+//        super.didMoveToWindow()
+//         numberLabel.text = "\(number)"
+//    }
+//
     
 
-    static let identifier = "gambitCellOne"
+    
 
-    func set(object gambit: Gambit) {
-         self.nameLabel.text = gambit.name
-         self.priceLabel.text = "\(gambit.price) ₽"
-         self.imageViewOne.downloaded(from: gambit.image)
-
-   }
-
-}
 
 
